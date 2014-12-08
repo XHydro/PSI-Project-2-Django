95a647b947cdd53eb52acf0cf22fc84afe6176f0
robi24 <robi240@gmail.com>
2014-12-08 23:31:51 +0100

    dodanie 1 czesci z pdf-a, coś o adminie

--- DjangoWebProject2/app/models.py
+++ DjangoWebProject2/app/models.py
@@ -3,5 +3,12 @@ Definition of models.
 """
 
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+
+class app(models.Model):
+    title = models.CharField(max_length = 150)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+
+admin.site.register(app)
